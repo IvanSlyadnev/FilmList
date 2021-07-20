6572e7448a26707ae3485d750241dbf2fc3ce8d8
IvanSlyadnev <ivanslyadnev2015@gmail.com>
2021-07-20 11:29:43 +0300

    commit Film model

---
+++ app/Models/Film.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Film extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id', 'name', 'year', 'budget', 'description', 'rate', 'length', 'user_id'
+    ];
+
+    public function creator () {
+        return $this->belongsTo(User::class);
+    }
+
+
+}
