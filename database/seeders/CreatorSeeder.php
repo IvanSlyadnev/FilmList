7df6237ad1c673a600c11b1bfd5e945f53db52c6
IvanSlyadnev <ivanslyadnev2015@gmail.com>
2021-07-22 17:39:27 +0300

    edit Seeder

--- database/seeders/CreatorSeeder.php
+++ database/seeders/CreatorSeeder.php
@@ -19,12 +19,14 @@ class CreatorSeeder extends Seeder
             [
                 'id' => 1,
                 'country_id' => 2,
-                'name' => 'Джоли'
+                'name' => 'Джоли',
+                'age' => 20
             ],
             [
                 'id' => 2,
                 'country_id' => 1,
-                'name' => 'Пит'
+                'name' => 'Пит',
+                'age' => 40
             ]
         ];
 
@@ -32,7 +34,8 @@ class CreatorSeeder extends Seeder
             DB::table('creators')->insert([
                'id' => $creator['id'],
                'country_id' => $creator['country_id'],
-               'name' => $creator['name']
+               'name' => $creator['name'],
+                'age' => $creator['age']
             ]);
         }
     }
