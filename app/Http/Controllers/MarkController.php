b4c3c23d22dec5b7da8d7bf049b761a832a7b16f
IvanSlyadnev <ivanslyadnev2015@gmail.com>
2021-07-27 17:17:47 +0300

    logger

--- app/Http/Controllers/MarkController.php
+++ app/Http/Controllers/MarkController.php
@@ -10,6 +10,7 @@ class MarkController extends Controller
     public function setMark(Request $request, Film $film) {
         $film->marks()->syncWithoutDetaching([$request->user()->id => ['value' => $request->mark]]);
         $film->update(['rate'=> $film->count_rate]);
+
         logger()->info($film->count_rate);
 
         return response(['id' => $film->id]);
