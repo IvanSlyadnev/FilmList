9af4c636a301199d9d6e345e7ed9450a5edd2486
IvanSlyadnev <ivanslyadnev2015@gmail.com>
2021-07-28 12:43:45 +0300

    edit request

--- app/Http/Requests/FilmRequest.php
+++ app/Http/Requests/FilmRequest.php
@@ -24,23 +24,23 @@ class FilmRequest extends FormRequest
     public function rules()
     {
         return [
-            'film.name' => 'required',
-            'film.year' => 'required',
-            'film.budget' => 'required',
-            'film.length' => 'required',
-            'film.countries' => 'array',
-            'film.creators' => 'array',
-            'film.creators.*.roles' => 'array',
-            'film.genres' => 'array'
+            'name' => 'required',
+            'year' => 'required',
+            'budget' => 'required',
+            'length' => 'required',
+            'countries' => 'array',
+            'creators' => 'array',
+            'creators.*.roles' => 'array',
+            'genres' => 'array'
         ];
     }
 
     public function messages() {
         return [
-            'film.name.required' => 'Введите имя',
-            'film.year.required' => 'Введите год',
-            'film.budget.required' => 'Введите бюджет',
-            'film.length.required' => 'Введите время фильма'
+            'name.required' => 'Введите имя',
+            'year.required' => 'Введите год',
+            'budget.required' => 'Введите бюджет',
+            'length.required' => 'Введите время фильма'
         ];
     }
 }
